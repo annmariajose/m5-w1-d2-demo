a5261a8ea6700a2dd0674856ac6b5f1adb685b39
Ann Maria Jose <annmariajose1412@gmail.com>
2024-07-02 12:50:07 -0500

    Add demo3

--- demo3.js
+++ demo3.js
@@ -1,5 +1,9 @@
 //functional example and adding Events
+function clickButton() {
+  alert("Clicked!!!!!!");
+}
+const myelement = (
+  <button onClick={clickButton}>Click the button!</button>
+);
 
-  
-  
-  ReactDOM.render(myelement, document.getElementById('root'));
+ReactDOM.render(myelement, document.getElementById('root'));
